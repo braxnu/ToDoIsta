17f5a28d7febe8e9d9e794aa13578b2b79fc3fd2
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2019-08-12 23:22:39 +0200

    Add changes

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
+var path = require('path')
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname, '/src/index.html'));
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+
+app.listen(3000, function (err) {
+    if(err){
+        console.log(err);
+    } else {
+        console.log('Example app listening on port 3000!'); 
+    }
 });
