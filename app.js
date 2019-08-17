b7b2b97c78faffe2198581fa6ddff75d3f0eb072
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2019-08-17 03:50:49 +0200

    Add changes

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 var express = require('express');
 var app = express();
-var path = require('path')
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'src/index.html'));
-});
+app.use('/', express.static('src'));
+//app.use(express.static('src'));  
 
 app.listen(3000, function (err) {
     if(err){
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-function createTodoList() {
+function createTodoList () {
     var item = document.getElementById("todoInput").value;
     newItem.appendChiild(item);
     var newItem = document.createElement("li");
     newItem.appendChild(text);
-    document.getElementById("createTodoItemButton").addEventListener("click", createTodoList);
+    document.getElementById("createTodoList").addEventListener("click", createTodoList);
 }
